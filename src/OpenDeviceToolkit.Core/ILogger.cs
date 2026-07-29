7686bb58e6ede2ec7716f067895a97f2149a40d3
jeridj1 <jeriddrako22@gmail.com>
2026-07-29 12:03:00 -0700

    Add structured logging abstraction

---
+++ src/OpenDeviceToolkit.Core/ILogger.cs
@@ -0,0 +1,8 @@
+namespace OpenDeviceToolkit.Core;
+
+public interface ILogger
+{
+    void Info(string message);
+    void Warning(string message);
+    void Error(string message, Exception? exception = null);
+}
