ddb938fc6f91b343890177390cd3b4df0bb446d9
jeridj1 <jeriddrako22@gmail.com>
2026-08-03 02:29:27 -0700

    Wire RP2040 PIO capture to DMA

--- firmware/rp2040-bridge/logic_capture.c
+++ firmware/rp2040-bridge/logic_capture.c
@@ -1,21 +1,35 @@
 #include "hardware/pio.h"
 #include "hardware/dma.h"
 #include "pico/stdlib.h"
+#include "logic_capture.pio.h"
 
-// PIO/DMA capture primitive. Higher-level decoding remains on the host.
-static const uint16_t capture_program_instructions[] = {
-    0x0001, // IN PINS, 1
-    0x0000  // JMP 0
-};
+static int dma_chan = -1;
+static PIO capture_pio = pio0;
+static uint capture_sm = 0;
 
-void odt_logic_capture(PIO pio, uint sm, uint pin, uint32_t *buffer, size_t words, uint sample_hz) {
-    (void)capture_program_instructions;
-    pio_gpio_init(pio, pin);
-    pio_sm_set_consecutive_pindirs(pio, sm, pin, 1, false);
-    pio_sm_set_enabled(pio, sm, false);
-    pio_sm_clear_fifos(pio, sm);
-    pio_sm_set_clkdiv(pio, sm, (float)clock_get_hz(clk_sys) / (float)sample_hz);
-    // DMA transfer wiring is the next firmware step; this establishes the capture contract.
-    (void)buffer;
-    (void)words;
+bool odt_logic_capture(uint pin, uint sample_hz, uint32_t *buffer, size_t words) {
+    if (pin > 29 || sample_hz == 0 || !buffer || words == 0) return false;
+    uint offset = pio_add_program(capture_pio, &odt_capture_program);
+    pio_sm_config c = odt_capture_program_get_default_config(offset);
+    sm_config_set_in_pins(&c, pin);
+    sm_config_set_in_shift(&c, false, true, 32);
+    float div = (float)clock_get_hz(clk_sys) / (float)sample_hz;
+    if (div < 1.0f) div = 1.0f;
+    sm_config_set_clkdiv(&c, div);
+    pio_gpio_init(capture_pio, pin);
+    pio_sm_set_consecutive_pindirs(capture_pio, capture_sm, pin, 1, false);
+    pio_sm_init(capture_pio, capture_sm, offset, &c);
+    pio_sm_clear_fifos(capture_pio, capture_sm);
+    if (dma_chan < 0) dma_chan = dma_claim_unused_channel(true);
+    dma_channel_config dc = dma_channel_get_default_config(dma_chan);
+    channel_config_set_transfer_data_size(&dc, DMA_SIZE_32);
+    channel_config_set_read_increment(&dc, false);
+    channel_config_set_write_increment(&dc, true);
+    channel_config_set_dreq(&dc, pio_get_dreq(capture_pio, capture_sm, false));
+    dma_channel_configure(dma_chan, &dc, buffer, &capture_pio->rxf[capture_sm], words, true);
+    pio_sm_set_enabled(capture_pio, capture_sm, true);
+    dma_channel_wait_for_finish_blocking(dma_chan);
+    pio_sm_set_enabled(capture_pio, capture_sm, false);
+    pio_remove_program(capture_pio, &odt_capture_program, offset);
+    return true;
 }
